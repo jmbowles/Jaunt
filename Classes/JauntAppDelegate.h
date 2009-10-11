2a3958ea1d54a324e9ed17d987c4634d81ff66d5
John M. Bowles <jmbowles@hughes.net>
2009-10-11 14:03:10 -0400

    Initial Xcode project

---
+++ Classes/JauntAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  JauntAppDelegate.h
+//  Jaunt
+//
+//  Created by John Bowles on 10/11/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JauntAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ Classes/JauntAppDelegate.m
@@ -0,0 +1,29 @@
+//
+//  JauntAppDelegate.m
+//  Jaunt
+//
+//  Created by John Bowles on 10/11/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import "JauntAppDelegate.h"
+
+@implementation JauntAppDelegate
+
+@synthesize window;
+
+
+- (void)applicationDidFinishLaunching:(UIApplication *)application {    
+
+    // Override point for customization after application launch
+    [window makeKeyAndVisible];
+}
+
+
+- (void)dealloc {
+    [window release];
+    [super dealloc];
+}
+
+
+@end
