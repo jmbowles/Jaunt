1c7e5079ea767aea08e0372fdc05945dab6e3b7e
John M. Bowles <jmbowles@hughes.net>
2009-10-18 08:24:07 -0400

    Refactored project to make trips view the first view displayed

---
+++ Classes/TripsController.h
@@ -0,0 +1,16 @@
+//
+//  TripsController.h
+//  Jaunt
+//
+//  Created by John Bowles on 10/18/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TripsController : UIViewController {
+
+}
+
+@end
